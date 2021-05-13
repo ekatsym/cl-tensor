944faac9c09afc15d6ee5830e8ed9a6c2d377256
ekatsym <ekatsymeee@gmail.com>
2021-05-14 02:28:45 +0900

    Renamed by removing cublas- and added BLAS functions level 2 and 3.

--- src/cublas/package.lisp
+++ src/cublas/package.lisp
@@ -2,72 +2,72 @@
   (:use :common-lisp :cffi)
   (:export
     ;; grovel types
-    #:+cublas-version+
+    #:+version+
     #:cu-complex
     #:cu-double-complex
-    #:cublas-handle
-    #:cublas-status
-    #:cublas-operation
-    #:cublas-fill-mode
-    #:cublas-diag-type
-    #:cublas-side-mode
-    #:cublas-pointer-mode
-    #:cublas-atomics-mode
-    #:cublas-gemm-algo
-    #:cublas-math
-    #:cublas-compute-type
-    #:cublas-log-callback
+    #:handle
+    #:status
+    #:operation
+    #:fill-mode
+    #:diag-type
+    #:side-mode
+    #:pointer-mode
+    #:atomics-mode
+    #:gemm-algo
+    #:math
+    #:compute-type
+    #:log-callback
     #:cuda-stream
 
     ;; library
     #:libcublas
 
     ;; cuBLAS helper functions
-    #:cublas-create
-    #:cublas-destroy
-    #:cublas-get-version
-    #:cublas-set-property
-    #:cublas-set-stream
-    #:cublas-get-stream
-    #:cublas-get-pointer-mode
-    #:cublas-set-pointer-mode
-    #:cublas-set-vector
-    #:cublas-get-vector
-    #:cublas-set-matrix
-    #:cublas-get-matrix
-    #:cublas-set-vector-async
-    #:cublas-get-vector-async
-    #:cublas-set-matrix-async
-    #:cublas-get-matrix-async
-    #:cublas-set-atomics-mode
-    #:cublas-get-atomics-mode
-    #:cublas-set-math-mode
-    #:cublas-get-math-mode
-    #:cublas-logger-configure
-    #:cublas-get-logger-callback
-    #:cublas-set-logger-callback
+    #:create
+    #:destroy
+    #:get-version
+    #:set-property
+    #:set-stream
+    #:get-stream
+    #:get-pointer-mode
+    #:set-pointer-mode
+    #:set-vector
+    #:get-vector
+    #:set-matrix
+    #:get-matrix
+    #:set-vector-async
+    #:get-vector-async
+    #:set-matrix-async
+    #:get-matrix-async
+    #:set-atomics-mode
+    #:get-atomics-mode
+    #:set-math-mode
+    #:get-math-mode
+    #:logger-configure
+    #:get-logger-callback
+    #:set-logger-callback
 
     ;; cuBLAS level-1 functions
-    #:cublas-isamax #:cublas-idamax
-    #:cublas-isamin #:cublas-idamin
-    #:cublas-saxpy #:cublas-daxpy #:cublas-caxpy #:cublas-zaxpy
-    #:cublas-scopy #:cublas-dcopy #:cublas-ccopy #:cublas-zcopy
-    #:cublas-sdot #:cublas-ddot #:cublas-cdot #:cublas-zdot
-    #:cublas-snrm2 #:cublas-dnrm2 #:cublas-cnrm2 #:cublas-znrm2
-    #:cublas-srot #:cublas-drot #:cublas-crot #:cublas-zrot
-    #:cublas-srotg #:cublas-drotg #:cublas-crotg #:cublas-zrotg
-    #:cublas-srotm #:cublas-drotm #:cublas-crotm #:cublas-zrotm
-    #:cublas-srotmg #:cublas-drotmg #:cublas-crotmg #:cublas-zrotmg
-    #:cublas-sscal #:cublas-dscal #:cublas-cscal #:cublas-zscal
-    #:cublas-sswap #:cublas-dswap #:cublas-cswap #:cublas-zswap
+    #:isamax #:idamax
+    #:isamin #:idamin
+    #:saxpy #:daxpy #:caxpy #:zaxpy
+    #:scopy #:dcopy #:ccopy #:zcopy
+    #:sdot #:ddot #:cdot #:zdot
+    #:snrm2 #:dnrm2 #:cnrm2 #:znrm2
+    #:srot #:drot #:crot #:zrot
+    #:srotg #:drotg #:crotg #:zrotg
+    #:srotm #:drotm #:crotm #:zrotm
+    #:srotmg #:drotmg #:crotmg #:zrotmg
+    #:sscal #:dscal #:cscal #:zscal
+    #:sswap #:dswap #:cswap #:zswap
 
     ;; cuBLAS level-2 functions
-    #:cublas-sgdmv #:cublas-dgdmv #:cublas-cgdmv #:cublas-zgdmv
-    #:cublas-sgemv #:cublas-dgemv #:cublas-cgemv #:cublas-zgemv
-    #:cublas-sger #:cublas-dger #:cublas-cger #:cublas-zger
-    #:cublas-ssbmv #:cublas-dsbmv #:cublas-csbmv #:cublas-zsbmv
-    #:cublas-sspmv #:cublas-dspmv #:cublas-cspmv #:cublas-zspmv
+    #:sgdmv #:dgdmv #:cgdmv #:zgdmv
+    #:sgemv #:dgemv #:cgemv #:zgemv
+    #:sger #:dger #:cger #:zger
+    #:ssbmv #:dsbmv #:csbmv #:zsbmv
+    #:sspmv #:dspmv #:cspmv #:zspmv
 
     ;; cuBLAS utility
-    #:with-cublas-handle
+    #:with-handle
     ))
